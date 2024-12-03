a83c332ef227d32cf61866504a563ce153b7a378
jhonatan peña <jhonatan2002pb21@gmail.com>
2024-12-02 21:37:27 -0500

    configuracion ci/cd

--- app/settings.py
+++ app/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Configuracion de la app
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
